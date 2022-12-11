5d451975233a4e30f38560d453df7a6213d13972
franck gaspoz <franck.gaspoz@gmail.com>
2022-12-11 15:07:57 +0100

    examples

--- AnsiVtConsole.NetCore.Examples/Program.cs
+++ AnsiVtConsole.NetCore.Examples/Program.cs
@@ -1,10 +1,28 @@
-﻿using cons = AnsiVtConsole.NetCore;
+﻿using System.Reflection;
+
+using cons = AnsiVtConsole.NetCore;
 
 var console = new cons.AnsiVtConsole();
 
+console.Out.Echoln($"(bon,f=cyan)AnsiVtConsole.NetCore v{Assembly.GetExecutingAssembly().GetName().Version}(br)");
+
+console.Out.Echoln("(uon,bon)init:(br)");
+
+console.Out.Echoln("(f=blue)using (f=darkgray)cons = AnsiVtConsole.NetCore;");
+console.Out.Echoln("(f=blue)var (f=cyan)console (f=darkgray)= (f=blue)new (f=darkgray)cons.(f=green)AnsiVtConsole(f=white)()(br)");
+
+console.Out.Echoln("(uon,bon)console.Infos():(br)");
 console.Infos();
 
-console.Out.Echo("(br,b=red,f=yellow,uon)Hello, World!(br)");
+console.Out.Echoln("(br,uon,bon)console.Out.Echo():");
+console.Out.Echoln("(br,b=red,f=yellow,uon)Hello, World!");
 
+console.Out.Echoln("(br,uon,bon)console.Logger:(br)");
+console.Logger.Log("console.Logger.Log()");
+console.Logger.LogWarning("console.Logger.LogWarning()");
+console.Logger.LogError("console.Logger.LogError()");
+console.Logger.LogException(
+    new ArgumentException("bad argument exception"),
+    "console.Logger.LogException():");
 
-//console.StdErr.WriteLine("hello world");
+console.Out.Echoln();
