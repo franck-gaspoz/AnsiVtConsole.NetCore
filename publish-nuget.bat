e38556d353e0e87861238fc345ee8e16181af321
franck gaspoz <franck.gaspoz@gmail.com>
2022-12-20 06:23:04 +0100

    v1.0.16 + publish script update

--- publish-nuget.bat
+++ publish-nuget.bat
@@ -3,14 +3,15 @@ cls
 echo --------------------------------------------------------------------------------------
 echo publish-nuget
 echo --------------------------------------------------------------------------------------
-echo tag tge version and publish the corresponding nuget from version to github and nuget
+echo tag the version and publish the corresponding nuget from version to github and nuget
 echo parameter: version tag
 echo exampple: publish-nuget 1.0.15
 echo nugetkey=%nugetkey%
 echo githubkey=%githubkey%
+echo version=%1%
 echo --------------------------------------------------------------------------------------
 
-rem git tag -a "v1.0.15.0" -m "v1.0.15.0";
-rem git push origin v1.0.15.0
+git tag -a "v%0%" -m "v%0%";
+git push origin v%0%
 rem nuget push AnsiVtConsole.NetCore/bin/Release/AnsiVtConsole.NetCore.1.0.15.nupkg oy2aew5pvvred4ogzazq2qqa45n36mfubanxwqjp2nxsli -Source https://api.nuget.org/v3/index.json -Verbosity detailed
 rem dotnet nuget push "AnsiVtConsole.NetCore/bin/Release/AnsiVtConsole.NetCore.1.0.15.nupkg" --api-key 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 --source "github"
