0b57ba10441fb38aeb85f2489570b0635129e49a
franck gaspoz <franck.gaspoz@gmail.com>
2022-12-20 06:25:08 +0100

    fix typo

--- publish-nuget.bat
+++ publish-nuget.bat
@@ -5,7 +5,7 @@ echo publish-nuget
 echo --------------------------------------------------------------------------------------
 echo tag the version and publish the corresponding nuget from version to github and nuget
 echo parameter: version tag
-echo exampple: publish-nuget 1.0.15
+echo example: publish-nuget 1.0.15
 echo nugetkey=%nugetkey%
 echo githubkey=%githubkey%
 echo version=%1%
