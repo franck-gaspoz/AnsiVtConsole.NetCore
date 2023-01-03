cb003c45da6d949ca1e444440c55d0856cece7c0
franck gaspoz <franck.gaspoz@gmail.com>
2023-01-03 13:08:50 +0100

    fix script

--- publish-nuget.bat
+++ publish-nuget.bat
@@ -11,7 +11,7 @@ echo githubkey=%githubkey%
 echo version=%1%
 echo --------------------------------------------------------------------------------------
 
-git tag -a "v%1%" -m "v%1%";
+git tag -a "v%1%" -m "v%1%"
 git push origin v%1%
 rem nuget push AnsiVtConsole.NetCore/bin/Release/AnsiVtConsole.NetCore.%1%.nupkg %nugetkey% -Source https://api.nuget.org/v3/index.json -Verbosity detailed
 rem dotnet nuget push "AnsiVtConsole.NetCore/bin/Release/AnsiVtConsole.NetCore.%1%.nupkg" --api-key %githubkey% --source "github"
