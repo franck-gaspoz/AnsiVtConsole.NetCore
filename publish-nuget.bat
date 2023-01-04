d15bcbb7ac0e5d99ad3072b8ebe6c08d7340c14c
franck gaspoz <franck.gaspoz@gmail.com>
2023-01-05 00:57:34 +0100

    v1.0.16

--- publish-nuget.bat
+++ publish-nuget.bat
@@ -8,10 +8,10 @@ echo parameter: version tag
 echo example: publish-nuget 1.0.15
 echo nugetkey=%nugetkey%
 echo githubkey=%githubkey%
-echo version=%1%
+echo version=%1
 echo --------------------------------------------------------------------------------------
 
-git tag -a "v%1%" -m "v%1%"
-git push origin v%1%
-rem nuget push AnsiVtConsole.NetCore/bin/Release/AnsiVtConsole.NetCore.%1%.nupkg %nugetkey% -Source https://api.nuget.org/v3/index.json -Verbosity detailed
-rem dotnet nuget push "AnsiVtConsole.NetCore/bin/Release/AnsiVtConsole.NetCore.%1%.nupkg" --api-key %githubkey% --source "github"
+git tag -a v%1 -m v%1
+git push origin v%1
+nuget push CommandLine.NetCore/bin/Release/CommandLine.NetCore.%1.nupkg %nugetkey% -SkipDuplicate -Source https://api.nuget.org/v3/index.json -Verbosity detailed
+dotnet nuget push "CommandLine.NetCore/bin/Release/CommandLine.NetCore.%1.nupkg" --skip-duplicate --api-key %githubkey% --source "github"
