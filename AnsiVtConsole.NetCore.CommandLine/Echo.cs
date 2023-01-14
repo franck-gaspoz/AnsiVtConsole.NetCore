4b4f069441c6f1bb6884ca85c52f6e793cbc7a91
franck gaspoz <franck.gaspoz@gmail.com>
2023-01-14 03:04:06 +0100

    impl echo command

--- AnsiVtConsole.NetCore.CommandLine/Echo.cs
+++ AnsiVtConsole.NetCore.CommandLine/Echo.cs
@@ -11,12 +11,22 @@ class Echo : Command
     }
 
     protected override CommandResult Execute(ArgSet args) =>
+
+        // echo <text> --err
+
         For(Param())
         .Do(() => DoEcho)
+        .Options(Opt("err"))
         .With(args);
 
-    void DoEcho(Param<string> text)
+    void DoEcho(Param<string> textParam, Opt errOpt)
     {
+        var console = new AnsiVtConsole();
+        var text = textParam.Value!;
 
+        if (errOpt.IsSet)
+            console.Out.WriteLine(text);
+        else
+            console.Logger.LogError(text);
     }
 }
