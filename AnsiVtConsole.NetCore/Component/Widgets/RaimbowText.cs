80f6f523ebe609f4cbcd253b2cc8254d9f15563f
franck gaspoz <franck.gaspoz@gmail.com>
2023-01-18 01:12:22 +0100

    adding widgets !

---
+++ AnsiVtConsole.NetCore/Component/Widgets/RaimbowText.cs
@@ -0,0 +1,44 @@
+﻿namespace AnsiVtConsole.NetCore.Component.Widgets;
+
+/// <summary>
+/// raimbow text
+/// </summary>
+public class RaimbowText : WidgetAbstact
+{
+    /// <summary>
+    /// text of the raimbow text
+    /// </summary>
+    public string? Text { get; private set; }
+
+    /// <summary>
+    /// raimbow text
+    /// </summary>
+    /// <param name="text">text</param>
+    public RaimbowText(string text)
+        => Text = text;
+
+    /// <summary>
+    /// raimbow text embeding a widget
+    /// </summary>
+    /// <param name="wrappedWidget">wrapped widget</param>
+    public RaimbowText(WidgetAbstact wrappedWidget)
+        : base(wrappedWidget) { }
+
+    /// <summary>
+    /// raimbow text at a fixed location
+    /// </summary>
+    /// <param name="text">text</param>
+    /// <param name="x">x</param>
+    /// <param name="y">y</param>
+    public RaimbowText(string text, int x, int y)
+        : base(x, y)
+            => Text = text;
+
+    /// <inheritdoc/>
+    public override string Render()
+    {
+        if (WrappedWidget is not null)
+            return base.Render();
+        return Text!;
+    }
+}
---
+++ AnsiVtConsole.NetCore/Component/Widgets/WidgetAbstact.cs
@@ -0,0 +1,45 @@
+﻿namespace AnsiVtConsole.NetCore.Component.Widgets;
+
+/// <summary>
+/// widget base class
+/// </summary>
+public abstract class WidgetAbstact
+{
+    /// <summary>
+    /// fixed location X if any else -1
+    /// </summary>
+    public int X { get; private set; } = -1;
+
+    /// <summary>
+    /// fixed location Y if any else -1
+    /// </summary>
+    public int Y { get; private set; }
+
+    /// <summary>
+    /// wrapped widget if any
+    /// </summary>
+    public WidgetAbstact? WrappedWidget { get; private set; }
+
+    /// <summary>
+    /// widget
+    /// </summary>
+    public WidgetAbstact(WidgetAbstact? wrappedWidget = null)
+        => WrappedWidget = wrappedWidget;
+
+    /// <summary>
+    /// widget at a fixed location
+    /// </summary>
+    /// <param name="x">cursor x</param>
+    /// <param name="y">cursor y</param>
+    public WidgetAbstact(int x, int y)
+        => (X, Y) = (x, y);
+
+    /// <summary>
+    /// render the widget
+    /// </summary>
+    /// <returns>the render of the widget</returns>
+    public virtual string Render()
+        => WrappedWidget is not null
+            ? WrappedWidget.Render()
+            : string.Empty;
+}
